1f067294c633ccbe97b5040a9a695cc50f4fe005
Samuel Amagbakhen <frontend_dev1@academystack.com>
2022-11-06 15:29:38 +0100

    initial

--- index.php
+++ index.php
@@ -10,38 +10,37 @@ header("Access-Control-Allow-Headers: X-Requested-With");
 $operation_type = $_POST['operation_type'];
 $first = $_POST['x'];
 $second = $_POST['y'];
+$result = 0;
 
-echo $first;
+switch ($operation_type) {
+    case "addition":
+        $result = $first + $second;
+        break;
+    case "subtraction":
+        $result = $first - $second;
+        break;
+    case "multiplication":
+        $result =  $first * $second;
+        break;
+    default:
+        $result = 0;
+}
 
-//function handleEnum($operationType, $firstValue, $secondValue){
-//
-//    switch ($operationType) {
-//        case "addition":
-//            return $firstValue + $secondValue;
-//        case "subtraction":
-//            return $firstValue - $secondValue;
-//        case "multiplication":
-//            return  $firstValue * $secondValue;
-//        default:
-//            return 0;
-//    }
-//}
-//
-//class Operation{
-//    public $slackUsername;
-//    public $result;
-//    public $operation_type;
-//
-//    //Methods
-//    function setAttributes($slackUsername, $operation_type, $firstValue, $secondValue){
-//        $this->slackUsername = $slackUsername;
-//        $this->result = handleEnum($operation_type, $firstValue, $secondValue);;
-//        $this->operation_type = $operation_type;;
-//    }
-//}
-//
-//$new_op = new Operation();
-//$tall_dev = $new_op->setAttributes("tall_dev", $operation_type, $first, $second);
-//$tall_dev_encoded = json_encode($tall_dev);
-//
-//echo $tall_dev_encoded;
+class Operation{
+    public $slackUsername;
+    public $result;
+    public $operation_type;
+
+    //Methods
+    function setAttributes($slackUsername, $operation_type, $result){
+        $this->slackUsername = $slackUsername;
+        $this->result = $result;
+        $this->operation_type = $operation_type;
+    }
+}
+
+$new_op = new Operation();
+$tall_dev = $new_op->setAttributes("tall_dev", $operation_type, $result);
+$tall_dev_encoded = json_encode($tall_dev);
+
+echo $tall_dev_encoded;
